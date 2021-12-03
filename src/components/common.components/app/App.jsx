277c5ddd93227eb1a2acbde7bb512c2b2f9b0c9d
ddb4 <ddb4@tpu.ru>
2021-12-03 14:16:08 +0700

    start create project

--- src/components/common.components/app/App.jsx
+++ src/components/common.components/app/App.jsx
@@ -1,19 +1,15 @@
 import Header from "../header/header";
-import FirstStartWebpack from "../../first-start-webpack/first-start-webpack";
-import StartReact from "../../start-react/start-react";
-import Recipes from "../../recipes/recipes";
-import StartBootstrap from "../../start-bootstrap/start-bootstrap";
+
 import Footer from "../footer/footer";
 
 const App = () => {
   return (
     <div className="wrapper">
-      <Header />
-      <FirstStartWebpack />
-      <StartReact />
-      <Recipes />
-      <StartBootstrap />
-      <Footer />
+      <main>
+        <Header />
+
+        <Footer />
+      </main>
     </div>
   );
 };
